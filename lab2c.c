213f2e9a54683983ce851785a457d8428eab80bb
MattG-UTA <mxg3697@mavs.uta.edu>
2026-03-05 15:12:04 -0600

    Add files via upload

--- lab2c.c
+++ lab2c.c
@@ -23,7 +23,8 @@ int main (int argc, char*argv[])
 	scanf("%d",&n);
 	n_original = n;
 	if((n-1)%2 != 0) n++;
-	m=n + ((n-1) / 2);  // Number of nodes in tree
+	m=n + (n-1) / 2;  // Number of nodes in tree
+	printf("%d\n", m);
 	priority=(double*) malloc(m*sizeof(double));
 	left=(int*) malloc(m*sizeof(int));
 	middle=(int*) malloc(m*sizeof(int));
@@ -39,23 +40,27 @@ int main (int argc, char*argv[])
 
 	minHeapInit(priority,m,m);
 
-	for (i=0;i<n;i++) priority[i]=(-1);
+	for (i=0;i<n;i++)
+	{ 
+		priority[i]=-1.0;
+		left[i]=middle[i]=right[i]=(-1);
+	}
 
 	// Read and load alphabet symbols' probabilities into priority queue.
 	probSum=0.0;
 	for (i=0;i<n_original;i++)
 	{
   		scanf("%lf",priority+i);
-  		probSum+=priority[i];
+  		probSum+=priority[i];	
   		minHeapInsert(i);
-  		left[i]=middle[i]=right[i]=(-1);
 	}
 	if(n > n_original) 
 	{
-		priority[n_original] = 0.0;
-		left[n_original] = middle[n_original] = right [n_original] =-1;
-		minHeapInsert(n_original);
+		priority[n_original - 1] = 0.0;
+		//left[n_original] = middle[n_original] = right[n_original] = -1;
+		minHeapInsert(n_original-1);
 	}
+	
 	printf("Probabilities sum to %f\n",probSum);
 
 	// Huffman code tree construction
@@ -69,36 +74,41 @@ int main (int argc, char*argv[])
   		minHeapInsert(i);
 	}
 
-	i=heapExtractMin();
+	//i=heapExtractMin();
 
-	if (i!=m-1)
+	/*if (i!=m-1)
 	{
   		printf("The root isn't the root\n");
   		exit(0);
-	}
+	}*/
 
-	parent[m-1]=(-1);
+	//heapExtractMin();
+	//parent[m-1]=(-1);
 
 	// Goes breadth-first from root to compute length of prefix bit codes.
 	length[m-1]=0;
 
-	for (i=m-1;i>=n;i--) length[left[i]] = length[middle[i]] = length[right[i]] = length[i] +1;
+	for (i=m-1;i>=n;i--) 
+	{
+		length[left[i]] = length[middle[i]] = length[right[i]] = length[i]+1;
+	}
+	//length[left[i]] = length[middle[i]] = length[right[i]] = length[i] +1;
 	
 	//length[left[i]]=length[middle[i]]=length[right[i]]=length[i]+1;
 
 	// Print the leaves, i.e. for the alphabet symbols
 	printf("   i  prob  parent  bits product  code\n");
 
-	for (i=0;i<n;i++)
+	for (i=0;i<n_original;i++)
 	{
   		// Crawl up the tree to get prefix code
   		outString[length[i]]='\0';
 
-  		for (j=i;j!=m-1;j=parent[j]) 
-  		if (left[parent[j]] == j)  outString[length[j-1]] = '0'; 
-  		else if(middle[parent[j]] == j) outString[length[j]] = '1'; 
+  		for (j=i;j!=m-1;j=parent[j]) {
+  		if (left[parent[j]] == j)  outString[length[j]-1] = '0'; 
+  		else if(middle[parent[j]] == j) outString[length[j]-1] = '1'; 
   		else outString[length[j]-1] = '2';
-  		
+  		}
   		//outString[length[j]-1]=(left[parent[j]]==j) ? '0' : '1';
 
   		printf("%5d %5.3f %5d %5d   %5.3f  %s\n", i,priority[i],parent[i],length[i],priority[i]*length[i],outString);
@@ -108,10 +118,10 @@ int main (int argc, char*argv[])
 	printf("Expected bits per symbol: %f\n",expected);
 
 	// Print the internal nodes
-	printf("   i  prob    left right parent\n");
+	printf("   i   prob   left    mid right parent\n");
 
 	for (i=n;i<m;i++)
-  	printf("%5d %5.3f %5d %5d  %5d\n",i,priority[i],left[i],right[i],parent[i]);
+  	printf("%5d %5.3f %5d %5d %5d  %5d\n",i,priority[i],left[i],middle[i],right[i],parent[i]);
 
 free(priority);
 free(left);
